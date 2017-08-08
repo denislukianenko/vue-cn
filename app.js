8bbc775909ff5f532fecd91a7d4e36ad4f80fc1a
denislukianenko <denlyk1@gmail.com>
2017-08-08 13:28:25 +0300

    )

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-new Vue({
+var one = new Vue({
     el: "#vue-app",
     data: {
-
+        health: 100,
+        ended: false
     },
     methods: {
-        logName: function(){
-            console.log('You entered your name')
-        },
-        logAge: function(){
-            console.log('You entered your age')
+       punch: function(){
+           this.health -= 10;
+           if (this.health <= 0) {
+               this.ended = true;
+           }
+       },
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
         }
+    },
+    computed: {
+
     }
 });
