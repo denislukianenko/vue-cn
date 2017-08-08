5ed31e4ab062e9def653475d9462bbf17e36a9b5
denislukianenko <denlyk1@gmail.com>
2017-08-08 16:09:01 +0300

    )

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 var one = new Vue({
-    el: "#vue-app",
+    el: "#vue-app-one",
     data: {
-        health: 100,
-        ended: false
+       
     },
     methods: {
-       punch: function(){
-           this.health -= 10;
-           if (this.health <= 0) {
-               this.ended = true;
-           }
-       },
-        restart: function() {
-            this.health = 100;
-            this.ended = false;
-        }
+
     },
     computed: {
 
